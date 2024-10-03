dbbe01509cbb39d6c883120fcc63e8df3a6d318f
Kain Nhantumbo <nhantumbok@gmail.com>
2024-10-04 01:25:36 +0200

    feat: added support to place the data under the user home directory

--- main.go
+++ main.go
@@ -1,14 +1,26 @@
 package main
 
 import (
+	"log"
+	"os"
+	"path/filepath"
+
 	"github.com/KainNhantumbo/go-notes-cli/commands"
 	"github.com/KainNhantumbo/go-notes-cli/storage"
 	"github.com/KainNhantumbo/go-notes-cli/todo"
 )
 
 func main() {
+	homeDir, err := os.UserHomeDir()
+
+	if err != nil {
+		log.Fatal("Could not find the user home directory: ", err)
+	}
+
+	var storagePath string = filepath.Join(homeDir, ".todos-data.json")
+
 	todosList := todo.TodosList{}
-	storage := storage.NewStorage[todo.TodosList]("internal-data.json")
+	storage := storage.NewStorage[todo.TodosList](storagePath)
 	storage.Load(&todosList)
 	cmdFlags := commands.CommandFlags{}
 	cmdFlags.Exec(&todosList)
