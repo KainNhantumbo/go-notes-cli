ac0b7efdf5f99fa30a23a5d19649c6042275549a
Kain Nhantumbo <nhantumbok@gmail.com>
2024-10-04 01:11:49 +0200

    feat: added setup to inialize the app

--- main.go
+++ main.go
@@ -1,6 +1,16 @@
 package main
 
+import (
+	"github.com/KainNhantumbo/go-notes-cli/commands"
+	"github.com/KainNhantumbo/go-notes-cli/storage"
+	"github.com/KainNhantumbo/go-notes-cli/todo"
+)
 
-func main () {
-
+func main() {
+	todosList := todo.TodosList{}
+	storage := storage.NewStorage[todo.TodosList]("internal-data.json")
+	storage.Load(&todosList)
+	cmdFlags := commands.CommandFlags{}
+	cmdFlags.Exec(&todosList)
+	storage.Save(todosList)
 }
---
+++ storage/storage.go
@@ -0,0 +1,34 @@
+package storage
+
+import (
+	"encoding/json"
+	"os"
+)
+
+type Storage[T interface{}] struct {
+	FileName string
+}
+
+func NewStorage[T interface{}](fileName string) *Storage[T] {
+	return &Storage[T]{FileName: fileName}
+}
+
+func (s *Storage[T]) Save(data T) error {
+	fileData, err := json.MarshalIndent(data, "", "    ")
+
+	if err != nil {
+		return err
+	}
+
+	return os.WriteFile(s.FileName, fileData, 0644)
+}
+
+func (s *Storage[T]) Load(data *T) error {
+	fileData, err := os.ReadFile(s.FileName)
+
+	if err != nil {
+		return err
+	}
+
+	return json.Unmarshal(fileData, data)
+}
